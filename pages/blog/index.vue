9ea0a281f70c1df25b2eab62c874c50b1a2cfc11
shibe97 <k.shibata97@gmail.com>
2020-01-12 17:52:45 +0900

    一覧ページを静的化

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -4,29 +4,28 @@
       <v-row justify="center">
         <h2>Blog</h2>
       </v-row>
-      <ul id="blogs"></ul>
+      <ul>
+        <li v-for="content in contents" :key="content.id">
+          <a :href="`/blog/${content.id}`">{{ content.title }}</a>
+        </li>
+      </ul>
     </section>
   </v-container>
 </template>
 <script>
+import axios from 'axios'
 export default {
-  mounted() {
-    fetch('https://tamasuke.microcms.io/api/v1/blog', {
-      headers: {
-        'X-API-KEY': '6cb11011-fa2f-4bd0-86b4-3fd32cc0c42a'
+  async asyncData({ params, error, payload }) {
+    const limit = 10
+    const { data } = await axios.get(
+      `https://tamasuke.microcms.io/api/v1/blog?limit=${limit}`,
+      {
+        headers: {
+          'X-API-KEY': '6cb11011-fa2f-4bd0-86b4-3fd32cc0c42a'
+        }
       }
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        // 画面表示
-        const list = json.contents
-          .map(
-            (content) =>
-              `<li><a href="/blog/${content.id}">${content.title}</a></li>`
-          )
-          .join('\n')
-        document.getElementById('blogs').innerHTML = list
-      })
+    )
+    return data
   }
 }
 </script>
